dbb516da533c6851493846985782e6e2b7d0e2c8
MDIMACat <romanmdima@gmail.com>
2023-11-18 21:32:09 +0200

    Removed unnecessary comments and put request on one line

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,21 +1,6 @@
 --Set up a test database for system
-
---Create a new database(test)
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
---Create a new user for database (test)
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' 
-IDENTIFIED BY 'hbnb_test_pwd';
-
---Grant hbnb_test read-only permissions to hbnb_test_db (test)
-GRANT SELECT
-ON performance_schema.*
-TO 'hbnb_test'@'localhost'
-
---Grant hbnb_test all privileges on hbnb_test_db (test)
-GRANT ALL PRIVILEGES
-ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-
--- Flush privileges to apply changes
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
