0346d9e829f99a9f5dd9303c3e50d40ae713659e
MDIMACat <romanmdima@gmail.com>
2023-11-20 17:16:18 +0200

    test db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,90 +1,116 @@
 #!/usr/bin/python3
-"""Module for Database Storage"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import scoped_session
+"""
+This module defines a class to manage
+database storage for hbnb clone
+"""
 from os import environ
-from models.amenity import Amenity
 from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.amenity import Amenity
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
 
 class DBStorage:
-    """Database class"""
-
+    """
+    This class manages database operations
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initialization of class attributes"""
-        user = environ.get('HBNB_MYSQL_USER')
-        password = environ.get('HBNB_MYSQL_PWD')
-        host = environ.get('HBNB_MYSQL_HOST', default='localhost')
-        database = environ.get('HBNB_MYSQL_DB')
-        env = environ.get('HBNB_ENV')
+        """ Initializes the engine """
+        user = environ.get("HBNB_MYSQL_USER")
+        pwd = environ.get("HBNB_MYSQL_PWD")
+        host = environ.get("HBNB_MYSQL_HOST", "localhost")
+        db = environ.get("HBNB_MYSQL_DB")
+        should_delete = environ.get("HBNB_ENV")
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
-                                      .format(user,
-                                              password,
-                                              host,
-                                              database), pool_pre_ping=True)
+        url = f"mysql://{user}:{pwd}@{host}/{db}"
+        self.__engine = create_engine(url, pool_pre_ping=True)
 
-        if env == 'test':
-            Base.metadata.drop_all(self.__engine)
+        if (should_delete == "tests"):
+            Base.metadata.drop_all(__engine)
+            return
 
     def all(self, cls=None):
-        """ Retrieve all the tables in the database
-        Args:
-            cls = Is the given class name to be queried
         """
-        new_dict = {}
+        Retrieves objects from the database.
 
-        clsname_dict = {
-            "User": User,
-            "State": State,
-            "Review": Review,
-            "Place": Place,
-            "City": City,
-            "Amenity": Amenity,
-        }
+        Parameters
 
-        if cls is not None and cls not in clsname_dict:
-            return new_dict
+            cls : class, optional
+            The class of objects to retrieve.
+            By default, all objects are fetched
 
-        if cls is not None:
-            objects = self.__session.query(clsname_dict[cls]).all()
-        else:
-            objects = []
-            for classes in clsname_dict.values():
-                objects.append(self.__session.query(classes).all())
+        Return
+            Objects matching the given class
+            otherwise all objects from the
+            database
+        """
+        models = {"State": State,
+                  "City": City,
+                  "Place": Place,
+                  "Review": Review,
+                  "User": User,
+                  "Amenity": Amenity}
+        my_dict = {}
+        objects = []
+
+        if (cls and cls.__name__ in models):
+            models = {"model": models.get(cls.__name__)}
+
+        for key, value in models.items():
+            objs = self.__session.query(value)
+            for obj in objs:
+                objects.append(obj)
+
         for obj in objects:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            value = obj
-            new_dict[key] = value
+            my_dict[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
-        return new_dict
+        return (my_dict)
 
     def new(self, obj):
-        """ Adds new objet to the database"""
+        """
+        Adds the given object to the database
+
+        Parameters
+            obj : class
+            The instance to be stored
+        """
         self.__session.add(obj)
 
     def save(self):
-        """Commits all chancges into the database"""
+        """ Commits changes to database """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database"""
-        if obj is not None:
+        """
+        Deletes the given object from the database
+
+        Parameters
+            obj : class, optional
+            The instance to delete. By default,
+            None is provided
+        """
+        if (obj):
             self.__session.delete(obj)
-        else:
-            return
 
     def reload(self):
-        """Creates all tables in the database"""
+        """ Creates all tables in the database """
+
         Base.metadata.create_all(self.__engine)
 
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)
+        session = sessionmaker(self.__engine,
+                               expire_on_commit=False)
+        self.__session = scoped_session(session)
+
+    def close(self):
+        """ Close """
+        self.__session.remove()
