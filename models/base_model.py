58f89b6d60cfa8d27f50a90dee941142ef385ca9
MDIMACat <romanmdima@gmail.com>
2023-11-20 17:23:33 +0200

    Orgiinal base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, String
 import uuid
 from datetime import datetime
+from os import environ
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
@@ -12,25 +13,26 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(String(60), primary_key=True)
+    id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if (kwargs):
-            for key, value in kwargs.items():
-                if (key == "__class__"):
-                    continue
-                setattr(self, key, datetime.fromisoformat(value)
-                        if ("_at" in key) else value)
+        """Instatntiates a new model"""
+        if 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
+        if 'created_at' not in kwargs:
+            self.created_at = datetime.now()
+        if 'updated_at' not in kwargs:
+            self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+            setattr(self, key, datetime.fromisoformat(value) if "_at" in key else value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = type(self).__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
@@ -42,17 +44,13 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = self.__dict__.copy()
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop("_sa_instance_state", "na")
-
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
-        """ Delete current instance from storage """
+        """ delete the current instance from the storage"""
+        from models import storage
         storage.delete(self)
