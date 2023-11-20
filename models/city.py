2c416cb8bc2d9cd4075ae8342f291ab2ab7feda4
MDIMACat <romanmdima@gmail.com>
2023-11-20 20:29:57 +0200

    Changed city class and state class

--- models/city.py
+++ models/city.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from os import environ
-
+import os
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-
-    __tablename__ = "cities"
-
-    if (environ.get("HBNB_TYPE_STORAGE", "file") == "db"):
-        name = Column(String(128), nullable=False)
-        state_id = Column(ForeignKey("states.id"),
-                          nullable=False)
-        places = relationship("Place", cascade="delete",
-                              backref="cities")
-    else:
-        name = ""
-        state_id = ""
+    __tablename__ = 'cities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(
+        String(60), ForeignKey('states.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
