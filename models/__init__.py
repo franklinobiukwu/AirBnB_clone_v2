d9481412d549f74d4fbaeb4d7dc9c36c8b759b2f
MDIMACat <romanmdima@gmail.com>
2023-11-21 20:58:43 +0200

    Task 6 and 2 fixed

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 
-
+print("Initializing")
 from os import environ
 
 storage_type = environ.get('HBNB_TYPE_STORAGE')
 storage = None
 
 if storage_type == 'db':
-    print("Of the DB!!")
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    print("Of the FileStorage")
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
--- models/state.py
+++ models/state.py
@@ -8,13 +8,19 @@ from sqlalchemy.orm import relationship
 
 storage_type = environ.get('HBNB_TYPE_STORAGE')
 
-
 class State(BaseModel, Base):
     """ State class """
     if storage_type == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City')
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
     else:
-    name = ""
+        __tablename__ = ''
+        name = ""
 
+        @property
+        def cities(self):
+            """Getter attribute for FileStorage"""
+            from models import storage
+            city_instances = storage.all('City')
+            return [city for city in city_instances.values() if city.state_id == self.id]
