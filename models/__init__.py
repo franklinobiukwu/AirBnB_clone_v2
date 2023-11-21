81f606f16e84cc34456f6b2bb45e0878c60aacaf
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-21 16:55:43 +0100

    File Storage working

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from os import environ
 
 
-storage = FileStorage()
+# storage = FileStorage()
+#
+print("I'm initializing")
+
+storage_type = environ.get('HBNB_TYPE_STORAGE')
+storage = None
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    print("Of to DB!!")
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    print("Of to FileStorage!!")
+    storage = FileStorage()
+
 storage.reload()
