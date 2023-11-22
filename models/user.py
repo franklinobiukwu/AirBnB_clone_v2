56eecdab29417bef0a65a31e17aa3a382ccf2cab
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-11-22 17:14:56 +0100

    Removed default

--- models/user.py
+++ models/user.py
@@ -15,8 +15,8 @@ class User(BaseModel, Base):
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True, default='')
-        last_name = Column(String(128), nullable=True, default='')
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship("Place", cascade="delete", backref="user")
         reviews = relationship('Review', cascade='delete', backref='user')
     else:
